872bf5ebe125a75565f7c18ccc569f31dc41625c
nathan <nathan.gordon.thomas@gmail.com>
2023-04-17 10:57:39 -0600

    Adds name colum to users table and includes name attribute to sing_up_params and account_update_params methods inside the RegistrationsController.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_161624) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
