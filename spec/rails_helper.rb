3fa983977745080977e87e5d97c3bdb6c0f7d75f
nathan <nathan.gordon.thomas@gmail.com>
2023-04-19 10:22:10 -0600

    Adds specs for collapsible_links_partial_path method.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,11 +13,6 @@ require 'capybara/poltergeist'
 require 'factory_girl_rails'
 require 'capybara/rspec'
 
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma 
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -49,6 +44,11 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryGirl::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma 
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
